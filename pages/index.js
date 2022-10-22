766924e7f9170c361bd0592bc99846e645d9af39
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-10-22 10:33:22 +0200

    Google tag

--- pages/index.js
+++ pages/index.js
@@ -20,15 +20,27 @@ export default function Home() {
   return (
     <React.Fragment>
       <Head>
-
-
-
         <title>RestorFX Billingstad - Bilpleie med ledende produkter i markedet i Asker og Bærum</title>
         <meta
           name="description"
           content="Vi er et team sertifiserte fagfolk innen bilpleie som spesialiserer seg på RestorFX lakkrestaurering og ClearFX lakkbeskyttelse. Asker og Bærum " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-W32FPPW');
+          `
+        }}
+      />
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W32FPPW"
+        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
       <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-R3W7NW1TXX"></Script>
 
       <Script id="google-analytics" strategy="afterInteractive">
@@ -51,6 +63,6 @@ export default function Home() {
         <Contact />
         <Footer />
       </main>
-    </React.Fragment>
+    </React.Fragment >
   )
 }
