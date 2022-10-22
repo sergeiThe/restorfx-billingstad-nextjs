2ef9b600fcd96086949583af59aa49223d852b61
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-10-22 10:13:42 +0200

    Analytics tag

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Contact from "../components/contact/Contact";
 import Messenger from "../components/contact/Messenger";
 import Footer from "../components/footer/Footer";
 import HeaderSection from "../components/header/HeaderSection";
-import Modal from "../components/modals/Modal";
+import Script from 'next/script'
 import ModalWindow from "../components/modals/ModalWindow";
 import Tjenester from "../components/services/Tjenester";
 import Head from "next/head";
@@ -20,12 +20,26 @@ export default function Home() {
   return (
     <React.Fragment>
       <Head>
+
+
+
         <title>RestorFX Billingstad - Bilpleie med ledende produkter i markedet i Asker og Bærum</title>
         <meta
           name="description"
           content="Vi er et team sertifiserte fagfolk innen bilpleie som spesialiserer seg på RestorFX lakkrestaurering og ClearFX lakkbeskyttelse. Asker og Bærum " />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-R3W7NW1TXX"></Script>
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-R3W7NW1TXX');
+      `}
+      </Script>
       <ModalWindow />
       <Messenger />
       <HeaderSection />
